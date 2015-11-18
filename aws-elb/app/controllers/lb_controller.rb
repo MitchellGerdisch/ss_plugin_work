16bc7707b9bae98186013a7bff2d2b42a6aaa69f
MitchellGerdisch <mitchell.gerdisch@rightscale.com>
2015-11-18 13:27:39 -0600

    d

--- aws-elb/app/controllers/lb_controller.rb
+++ aws-elb/app/controllers/lb_controller.rb
@@ -55,7 +55,8 @@ module V1
         response.headers['Content-Type'] = 'application/json'
         response.body = resp_body
         app.logger.info("success during show - response body: "+response.body.to_s)
-      rescue  Aws::ElasticLoadBalancing::Errors::AccessPointNotFoundException,
+      rescue  Aws::ElasticLoadBalancing::Errors::LoadBalancerNotFound,
+              Aws::ElasticLoadBalancing::Errors::AccessPointNotFoundException,
               Aws::ElasticLoadBalancing::Errors::InvalidEndPointException => e
         response = Praxis::Responses::BadRequest.new()
         response.body = { error: e.inspect }
