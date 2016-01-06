e5810071eef603779f000dbdb3aa38af66baf82b
MitchellGerdisch <mitchell.gerdisch@rightscale.com>
2016-01-06 17:53:19 -0600

    tweaks for ports

--- aws-elb/run_plugin.sh
+++ aws-elb/run_plugin.sh
@@ -1,4 +1,4 @@
 export API_SHARED_SECRET='PUT_SHARED_SECRET_HERE'
 
-# Assumes its listening on port 8888
-bundle exec rackup -p 8888 -o SERVER_IP
+# See config.ru for port setting
+bundle exec rackup -o SERVER_IP
--- aws-rds/run_plugin.sh
+++ aws-rds/run_plugin.sh
@@ -1,4 +1,4 @@
 export API_SHARED_SECRET='PUT_SHARED_SECRET_HERE'
 
-# assumes it's listening on port 8080
-bundle exec rackup -p 8080 -o SERVER_IP
+# See config.ru for port setting
+bundle exec rackup -o SERVER_IP
