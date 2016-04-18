76a27c39cdd9218cd0e41cb8bf5913875e9d9b51
MitchellGerdisch <mitchell.gerdisch@rightscale.com>
2016-04-18 12:35:55 -0500

    media types bug fix

--- aws-rds/design/media_types/rds_mediatypes.rb
+++ aws-rds/design/media_types/rds_mediatypes.rb
@@ -5,31 +5,13 @@ module V1
       identifier 'application/json'
 
       attributes do
-        attribute :id, Attributor::String
-        attribute :href, Attributor::String
-        attribute :name, Attributor::String
-        attribute :allocated_storage, Attributor::Integer
-        attribute :major_version_upgrade, Attributor::String
-        attribute :minor_version_upgrade, Attributor::String
-        attribute :backup_retention_period, Attributor::Integer
-        attribute :instance_class, Attributor::String
+        attribute :db_name, Attributor::String
         attribute :instance_id, Attributor::String
+        attribute :instance_class, Attributor::String
         attribute :engine, Attributor::String
-        attribute :engine_version, Attributor::String
-        attribute :license_model, Attributor::String
+        attribute :allocated_storage, Attributor::Integer
         attribute :master_username, Attributor::String
         attribute :master_user_password, Attributor::String
-        attribute :multi_az, Attributor::String
-        attribute :port, Attributor::Integer
-        attribute :preferred_backup_window, Attributor::String
-        attribute :preferred_maintenance_window, Attributor::String
-        attribute :deletion_policy, Attributor::String
-        attribute :publicly_accessible, Attributor::String
-        attribute :storage_encrypted, Attributor::String
-        attribute :storage_type, Attributor::String
-        attribute :vpc_secgroups, Attributor::Collection.of(String)
-        attribute :subnet_group_name, Attributor::String
-        attribute :tags, Attributor::Collection.of(String)
         attribute :aws_creds, Attributor::Collection.of(String)
 
       end
