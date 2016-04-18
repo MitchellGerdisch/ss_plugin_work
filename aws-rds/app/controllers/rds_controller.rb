228db657dfbc3197c7034e319e07f55375fc430b
MitchellGerdisch <mitchell.gerdisch@rightscale.com>
2016-04-18 14:11:40 -0500

    fix

--- aws-rds/app/controllers/rds_controller.rb
+++ aws-rds/app/controllers/rds_controller.rb
@@ -21,6 +21,8 @@ module V1
         list_rds_response = rds.describe_db_instances
 
         list_rds_response.db_instances.each do |db_instance|
+          Praxis::Application.instance.logger.info "db_instance from AWS: "+db_instance.to_s
+
           my_db_instances << { 
             "db_instance_id": db_instance.db_instance_identifier,
             "db_name": db_instance["db_name"],
